32b16791898b5f93ed616210073ffaf0c537bdce
NG CHUN HOU <chunhou574197@gmail.com>
2021-08-17 22:25:42 +0800

    comment out unneccessary functions

--- classes/pages/index.php
+++ classes/pages/index.php
@@ -279,91 +279,91 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/gamestation/env.php';
         }
 
         // Generate News Template 
-        protected function makeNewsTemplate($day)
-        {
-              for ($i = 0; $i < count($this->ListTable[$day]); $i++) {
-                file_put_contents($this->rewriteNewsTitleUrl($this->ListTable[$day][$i]['news_title']),
-                 "
-                 <head>
-                  <title>" . ucfirst($this->ListTable[$day][$i]['news_title']) . "</title>
-                  <link rel='icon' type='image/png' href='/gamestation/view/assets/images/meteor-light-resized.svg' size='16x16'>
-                  <script type='text/javascript' src='/gamestation/view/assets/js/jquery-3.4.1.min.js'></script>
-                  <link rel='stylesheet' href='/gamestation/view/assets/css/bootstrap.min.css'>
-                  <link rel='stylesheet' href='/gamestation/view/assets/css/main.css'>
-                  <script type='text/javascript' src='/gamestation/view/assets/js/bootstrap.min.js'></script>
-                  <script type='text/javascript' src='/gamestation/node_modules/animejs/lib/anime.min.js'></script>
-                </head>
-                <header>
-                    <div class='container-fluid'>
-                    <div class='row'>
-                        <div class='col-md-12 rem-pad'>
-                            <div class='navbar navbar-expand-md navbar-dark bg-logo'>
-                                <div class='logo navbar-brand'><img rel='preload' src='/gamestation/view/assets/images/meteor-light-resized.svg' alt='logo' width='32'></div>
-                                <button class='navbar-toggler' data-toggle='collapse' data-target='#headerlist'>
-                                    <span class='navbar-toggler-icon'></span>
-                                </button>
-                                <div class='collapse navbar-collapse' id='headerlist'>
-                                <ul class='navbar-nav header-list' data-iscapitailse='true'>
-                                    <li class='nav-item'>
-                                        <a class='nav-link' href='/gamestation' target='_self'>HOME</a>
-                                    </li>
-                                    <li class='nav-item'>
-                                        <a class='nav-link' href='/gamestation/about' target='_self'>ABOUT US</a>
-                                    </li>
-                                    <li class='nav-item'>
-                                        <a class='nav-link' href='/gamestation/news' target='_self'>NEWS</a>
-                                    </li>
-                                </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                  </div>
-                </header>
-                  <div class='container'>
-                    <div class='row'>
-                            <div class='col-md-12 mainHeading'>
-                  "
-                      . 
-                      '<h3>'. ucfirst($this->ListTable[$day][$i]['news_title']) . '</h3>'
-                      .
-                    "
-                          </div>
-                    </div>
-                    <div class='row'>
-                      <div class='col-12 mt-4 mb-4'>
-                    "
-                      .
-                       "<img srcset=".$this->ListTable[$day][$i]['imgNews_content']. " class='article-img' alt='article image'> 
-                       "
-                      .
-                    "
-                      </div>
-                    </div>
-                    <div class='row'>
-                      <div class='col-10 col-md-10 col-lg-8 card' style='padding: 15px; margin: 15px;'>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                      </div>
-                    </div>
-                  </div>
-                  ");
+        // protected function makeNewsTemplate($day)
+        // {
+        //       for ($i = 0; $i < count($this->ListTable[$day]); $i++) {
+        //         file_put_contents($this->rewriteNewsTitleUrl($this->ListTable[$day][$i]['news_title']),
+        //          "
+        //          <head>
+        //           <title>" . ucfirst($this->ListTable[$day][$i]['news_title']) . "</title>
+        //           <link rel='icon' type='image/png' href='/gamestation/view/assets/images/meteor-light-resized.svg' size='16x16'>
+        //           <script type='text/javascript' src='/gamestation/view/assets/js/jquery-3.4.1.min.js'></script>
+        //           <link rel='stylesheet' href='/gamestation/view/assets/css/bootstrap.min.css'>
+        //           <link rel='stylesheet' href='/gamestation/view/assets/css/main.css'>
+        //           <script type='text/javascript' src='/gamestation/view/assets/js/bootstrap.min.js'></script>
+        //           <script type='text/javascript' src='/gamestation/node_modules/animejs/lib/anime.min.js'></script>
+        //         </head>
+        //         <header>
+        //             <div class='container-fluid'>
+        //             <div class='row'>
+        //                 <div class='col-md-12 rem-pad'>
+        //                     <div class='navbar navbar-expand-md navbar-dark bg-logo'>
+        //                         <div class='logo navbar-brand'><img rel='preload' src='/gamestation/view/assets/images/meteor-light-resized.svg' alt='logo' width='32'></div>
+        //                         <button class='navbar-toggler' data-toggle='collapse' data-target='#headerlist'>
+        //                             <span class='navbar-toggler-icon'></span>
+        //                         </button>
+        //                         <div class='collapse navbar-collapse' id='headerlist'>
+        //                         <ul class='navbar-nav header-list' data-iscapitailse='true'>
+        //                             <li class='nav-item'>
+        //                                 <a class='nav-link' href='/gamestation' target='_self'>HOME</a>
+        //                             </li>
+        //                             <li class='nav-item'>
+        //                                 <a class='nav-link' href='/gamestation/about' target='_self'>ABOUT US</a>
+        //                             </li>
+        //                             <li class='nav-item'>
+        //                                 <a class='nav-link' href='/gamestation/news' target='_self'>NEWS</a>
+        //                             </li>
+        //                         </ul>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //           </div>
+        //         </header>
+        //           <div class='container'>
+        //             <div class='row'>
+        //                     <div class='col-md-12 mainHeading'>
+        //           "
+        //               . 
+        //               '<h3>'. ucfirst($this->ListTable[$day][$i]['news_title']) . '</h3>'
+        //               .
+        //             "
+        //                   </div>
+        //             </div>
+        //             <div class='row'>
+        //               <div class='col-12 mt-4 mb-4'>
+        //             "
+        //               .
+        //                "<img srcset=".$this->ListTable[$day][$i]['imgNews_content']. " class='article-img' alt='article image'> 
+        //                "
+        //               .
+        //             "
+        //               </div>
+        //             </div>
+        //             <div class='row'>
+        //               <div class='col-10 col-md-10 col-lg-8 card' style='padding: 15px; margin: 15px;'>
+        //                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        //               </div>
+        //             </div>
+        //           </div>
+        //           ");
                 // Moving news articles into newsArticle directory
-                rename($this->rewriteNewsTitleUrl($this->ListTable[$day][$i]['news_title']), $_SERVER['DOCUMENT_ROOT']."/gamestation/view/newsArticle/".$this->rewriteNewsTitleUrl($this->ListTable[$day][$i]['news_title']).".php");
-              }
-        }
+        //         rename($this->rewriteNewsTitleUrl($this->ListTable[$day][$i]['news_title']), $_SERVER['DOCUMENT_ROOT']."/gamestation/view/newsArticle/".$this->rewriteNewsTitleUrl($this->ListTable[$day][$i]['news_title']).".php");
+        //       }
+        // }
 
-        public function createArticleContent()
-        {
-            // loading article content
-            if ($_SERVER['REQUEST_URI'] == '/news')
-            {
-              $this->makeNewsTemplate("monday");
-              $this->makeNewsTemplate("tuesday");
-              $this->makeNewsTemplate("wednesday");
-              $this->makeNewsTemplate("thursday");
-              $this->makeNewsTemplate("friday");
-              $this->makeNewsTemplate("saturday");
-              $this->makeNewsTemplate("sunday");
+        // public function createArticleContent()
+        // {
+        //     // loading article content
+        //     if ($_SERVER['REQUEST_URI'] == '/news')
+        //     {
+        //       $this->makeNewsTemplate("monday");
+        //       $this->makeNewsTemplate("tuesday");
+        //       $this->makeNewsTemplate("wednesday");
+        //       $this->makeNewsTemplate("thursday");
+        //       $this->makeNewsTemplate("friday");
+        //       $this->makeNewsTemplate("saturday");
+        //       $this->makeNewsTemplate("sunday");
               // for ($i = 0; $i < count($this->ListTable['tuesday']); $i++) {
               //   // fopen($this->rewriteNewsTitleUrl($this->ListTable['tuesday'][$i]['news_title']), "w");
               //   file_put_contents($this->rewriteNewsTitleUrl($this->ListTable['tuesday'][$i]['news_title']),
@@ -384,8 +384,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] .'/gamestation/env.php';
               //    ");
               //   rename($this->rewriteNewsTitleUrl($this->ListTable['tuesday'][$i]['news_title']), $_SERVER['DOCUMENT_ROOT']."/gamestation/view/newsArticle/".$this->rewriteNewsTitleUrl($this->ListTable['tuesday'][$i]['news_title']).".php");
               // }
-            }
-        }
+        //     }
+        // }
     }
 
 ?>
